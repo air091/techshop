1a7e49a1ede36ae4bd7a6c82d3dfc90e8d7070f8
Air <acesparkle243@gmail.com>
2025-12-04 20:30:58 +0800

    frontend(2)

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const Register = () => {
   const [user, setUser] = useState({ email: "", password: "" });
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -23,7 +24,8 @@ const Register = () => {
         setError(data.message);
         return;
       }
-      console.log(data);
+      setSuccess(data.message);
+      setUser(data.user);
     } catch (error) {
       console.error(`Register API ${error}`);
     } finally {
@@ -31,7 +33,77 @@ const Register = () => {
     }
   };
 
-  return <div>Register Page</div>;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (confirmPassword !== user.password) {
+      setError("Password do not match.");
+      return;
+    }
+
+    await registerAPI();
+    setUser({ email: "", password: "" });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return (
+    <>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <header>
+            <h1>Register your Account</h1>
+          </header>
+          <form onSubmit={handleSubmit}>
+            {error && <p className="bg-red-200">{error}</p>}
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                id="email"
+                type="email"
+                autoComplete="off"
+                name="email"
+                value={user.email}
+                onChange={handleChange}
+                className="border block"
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                id="password"
+                type="password"
+                autoComplete="off"
+                name="password"
+                value={user.password}
+                onChange={handleChange}
+                className="border block"
+              />
+            </div>
+            <div>
+              <label htmlFor="confirm-password">Confirm password</label>
+              <input
+                id="confirm-password"
+                type="password"
+                name="confirm-password"
+                value={user.value}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                className="block border"
+              />
+            </div>
+            <button type="submit" className="block border px-2 cursor-pointer">
+              Register
+            </button>
+          </form>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Register;
